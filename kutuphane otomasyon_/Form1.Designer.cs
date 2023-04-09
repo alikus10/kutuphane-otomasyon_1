820f2f164010cc1ab0c578a5dfbe874d3676ca8d
alikus10 <kusali938@gmail.com>
2023-04-09 23:55:31 +0300

    Proje dosyası ekle.

--- kutuphane otomasyon_/Form1.Designer.cs
+++ kutuphane otomasyon_/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.eposta_txt = new System.Windows.Forms.TextBox();
+            this.sifre_txt = new System.Windows.Forms.TextBox();
+            this.giris_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "E-posta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Şifre";
+            // 
+            // eposta_txt
+            // 
+            this.eposta_txt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.eposta_txt.Location = new System.Drawing.Point(123, 195);
+            this.eposta_txt.Name = "eposta_txt";
+            this.eposta_txt.Size = new System.Drawing.Size(100, 22);
+            this.eposta_txt.TabIndex = 1;
+            // 
+            // sifre_txt
+            // 
+            this.sifre_txt.Location = new System.Drawing.Point(123, 232);
+            this.sifre_txt.Name = "sifre_txt";
+            this.sifre_txt.Size = new System.Drawing.Size(100, 22);
+            this.sifre_txt.TabIndex = 2;
+            // 
+            // giris_btn
+            // 
+            this.giris_btn.Location = new System.Drawing.Point(30, 286);
+            this.giris_btn.Name = "giris_btn";
+            this.giris_btn.Size = new System.Drawing.Size(193, 50);
+            this.giris_btn.TabIndex = 2;
+            this.giris_btn.Text = "GİRİş1";
+            this.giris_btn.UseVisualStyleBackColor = true;
+            this.giris_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::kutuphane_otomasyon_.Properties.Resources._240_F_301065920_3OPfIY9SHSIHxQbImyYOyqjGeevKZlwT;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(193, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(252, 355);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.giris_btn);
+            this.Controls.Add(this.sifre_txt);
+            this.Controls.Add(this.eposta_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gümüşhane Kütüphanesi";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox eposta_txt;
+        private System.Windows.Forms.TextBox sifre_txt;
+        private System.Windows.Forms.Button giris_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
