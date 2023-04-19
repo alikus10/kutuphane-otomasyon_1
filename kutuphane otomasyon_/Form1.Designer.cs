d7b5616f392e3e7afb18e156d3fe2f7d50a9b86c
furkangokoluk <furkangokoluk@gmail.com>
2023-04-20 00:09:30 +0300

    Veritabanı (Firebase) ile proje birbirleriyle ilişkilendirildi!

--- kutuphane otomasyon_/Form1.Designer.cs
+++ kutuphane otomasyon_/Form1.Designer.cs
@@ -33,9 +33,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.eposta_txt = new System.Windows.Forms.TextBox();
             this.sifre_txt = new System.Windows.Forms.TextBox();
-            this.giris_btn = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.girisBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -70,26 +68,16 @@
             this.sifre_txt.Size = new System.Drawing.Size(127, 22);
             this.sifre_txt.TabIndex = 2;
             // 
-            // giris_btn
+            // girisBtn
             // 
-            this.giris_btn.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.giris_btn.Location = new System.Drawing.Point(36, 238);
-            this.giris_btn.Name = "giris_btn";
-            this.giris_btn.Size = new System.Drawing.Size(190, 50);
-            this.giris_btn.TabIndex = 2;
-            this.giris_btn.Text = "GİRİŞ";
-            this.giris_btn.UseVisualStyleBackColor = false;
-            this.giris_btn.Click += new System.EventHandler(this.giris_btn_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::kutuphane_otomasyon_.Properties.Resources._240_F_301065920_3OPfIY9SHSIHxQbImyYOyqjGeevKZlwT;
-            this.pictureBox1.Location = new System.Drawing.Point(33, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(193, 114);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.girisBtn.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.girisBtn.Location = new System.Drawing.Point(36, 238);
+            this.girisBtn.Name = "girisBtn";
+            this.girisBtn.Size = new System.Drawing.Size(190, 50);
+            this.girisBtn.TabIndex = 2;
+            this.girisBtn.Text = "GİRİŞ";
+            this.girisBtn.UseVisualStyleBackColor = false;
+            this.girisBtn.Click += new System.EventHandler(this.girisBtn_Click);
             // 
             // GirisEkranı
             // 
@@ -98,8 +86,7 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(259, 300);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.giris_btn);
+            this.Controls.Add(this.girisBtn);
             this.Controls.Add(this.sifre_txt);
             this.Controls.Add(this.eposta_txt);
             this.Controls.Add(this.label2);
@@ -109,7 +96,6 @@
             this.Name = "GirisEkranı";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Kullanıcı Girişi";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,8 +107,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox eposta_txt;
         private System.Windows.Forms.TextBox sifre_txt;
-        private System.Windows.Forms.Button giris_btn;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button girisBtn;
     }
 }
 
--- kutuphane otomasyon_/Program.cs
+++ kutuphane otomasyon_/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace kutuphane_otomasyon_
 {
@@ -14,9 +15,16 @@ namespace kutuphane_otomasyon_
         [STAThread]
         static void Main()
         {
+            XmlDocument config = new XmlDocument();
+            config.Load("config.xml");
+
+            string AuthDomain = config.DocumentElement.SelectSingleNode("/FireBase/AuthDomain").InnerText.Trim();
+            string ApiKey = config.DocumentElement.SelectSingleNode("/FireBase/ApiKey").InnerText.Trim();
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GirisEkranı());
+            Application.Run(new GirisEkranı(AuthDomain, ApiKey));
         }
     }
 }
